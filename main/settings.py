b10da82fea4fd62ee07b220d49c7ad579e6e2a6a
Pratham Gupta <f20190051@pilani.bits-pilani.ac.in>
2020-01-24 20:15:11 +0530

    updated models

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prathamguptaecommerceproject.herokuapp.com']
+ALLOWED_HOSTS = ['prathamguptaecommerceproject.herokuapp.com','127.0.0.1']
 
 
 # Application definition
